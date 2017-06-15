6a590bb03b0bd09cf56226d3b5b7f930dd619c87
Tendai Mzondo <mzondot@hotmail.com>
2017-06-15 16:28:20 +0200

    initial upload

---
+++ XamarinAllianceApp/Views/CharacterPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinAllianceApp.Models;
+
+namespace XamarinAllianceApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CharacterPage : ContentPage
+    {
+        public Character _charac { get; set; }
+        public CharacterPage()
+        {
+            InitializeComponent();         
+           
+        }
+        public CharacterPage(Character character):this()
+        {
+            _charac = character;
+            BindingContext = _charac;
+        }
+
+        
+    }
+}
